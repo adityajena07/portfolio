fafb48daf7737625a8e2a7342c23afb0873087e7
adityajena07 <jena22793@gmail.com>
2024-09-11 23:43:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let sections = document.querySelector('section');
-let navlinks = document.querySelector('header nav a');
+let sections = document.querySelectorAll('section');  // Use querySelectorAll to select all sections
+let navlinks = document.querySelectorAll('header nav a');  // Use querySelectorAll to select all nav links
 
 window.onscroll = () => {
-    sections.array.forEach(sec => {
+    sections.forEach(sec => {
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navlinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*' + id + ' ]').classList.add('active')
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                links.classList.remove('active');  // Remove 'active' class from all links
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')  // Add 'active' class to the correct link
             })
         }
-    });
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
